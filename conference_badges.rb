65a0d89e941dc2c091f28ff8e42276d57cf44a69
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-15 19:10:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendees|
+    return "Hello, my name is #{attendees}."
+  end
+end
+
+def assign_rooms
+end
