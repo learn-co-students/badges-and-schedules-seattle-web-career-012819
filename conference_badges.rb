aa50f761a39fa927c97b7a46fb5e362b8817d5eb
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-15 19:15:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  room_number = 1 
+  room_number = 0 
   attendees.collect do |name|
     room_number += 1 
     "Hello, #{name}! You'll be assigned to room #{room_number}!"
