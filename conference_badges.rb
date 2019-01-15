7de810e0748d31f48176c2e960fa152932e99674
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-15 19:11:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendees|
+  attendees.collect do |name|
     return "Hello, my name is #{attendees}."
   end
 end
