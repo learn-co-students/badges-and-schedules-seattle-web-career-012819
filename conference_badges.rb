e9c7d51c7e152809d50dcb11d0ebfe844013a10e
yong liang <yongliang24@gmail.com>
2019-01-20 03:25:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(n)
+  return "Hello, my name is #{n}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+
+def assign_rooms(array)
+  new_array = []
+  room_num = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{room_num}!")
+    room_num += 1
+  end
+  return new_array
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |index|
+    puts index
+  end
+
+  assign_rooms(array).each do |index|
+    puts index
+  end
+end
