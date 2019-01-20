3a21bf9a72dc225dce282a2cb7634e15a453b3ec
tiefengeist <sahirnambiar@gmail.com>
2019-01-20 14:53:56 -0800

    Finished

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_dispatches = []
+  array.each {|i| badge_dispatches.push(badge_maker(i))}
+  badge_dispatches.each {|i| puts i}
+end
+
+def assign_rooms(names)
+  names.map.with_index do |name, int|
+    "Hello, #{name}! You'll be assigned to room #{int + 1}!"
+  end
+end
+
+def printer(names)
+  batch_badge_creator(names)
+  assign_rooms(names).each {|x| puts x}
+end
